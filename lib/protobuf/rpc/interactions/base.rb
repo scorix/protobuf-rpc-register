9c844e36eaa60c58a6184da892fad9b3aea9efaa
scorix <scorix@gmail.com>
2016-05-19 17:42:07 +0800

    Reject nil values

--- lib/protobuf/rpc/interactions/base.rb
+++ lib/protobuf/rpc/interactions/base.rb
@@ -4,6 +4,11 @@ module Protobuf
   module Rpc
     module Interactions
       class Base < ::ActiveInteraction::Base
+        def presence_inputs
+          # todo: need more fields to let server know specified nil values, otherwise it will be ignored
+          inputs.reject { |k, v| !given?(k) && v.nil? }
+        end
+
         def self.except_attributes
           [].freeze
         end
