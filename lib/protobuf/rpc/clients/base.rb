21a2a0eba0eb903026e6bddd314e516a3ae78ac4
scorix <scorix@gmail.com>
2016-06-20 10:26:50 +0800

    always use ::Protobuf

--- lib/protobuf/rpc/clients/base.rb
+++ lib/protobuf/rpc/clients/base.rb
@@ -26,9 +26,9 @@ module Protobuf
         def send_rpc_request(method, msg, serializer:)
           res = nil
 
-          internal_client.send(method, Serializer.dump(msg, serializer: serializer)) do |c|
+          internal_client.send(method, ::Protobuf::Rpc::Serializer.dump(msg, serializer: serializer)) do |c|
             c.on_success do |rpc_compressed_message|
-              res = Protobuf::Rpc::Serializer.load(rpc_compressed_message)
+              res = ::Protobuf::Rpc::Serializer.load(rpc_compressed_message)
             end
 
             c.on_failure do |error|
@@ -41,14 +41,14 @@ module Protobuf
 
         def check_response_error(res, raise_error: true)
           case res
-            when Protobuf::Rpc::Messages::Error
+            when ::Protobuf::Rpc::Messages::Error
               error_class = self.class.mutex.synchronize { define_error_class(res) }
               error = error_class.new(res.error_message)
               error.set_backtrace(res.error_backtrace)
               raise_error(error, raise_error)
-            when Protobuf::Error, Protobuf::Rpc::ClientError
-              error_reason = Protobuf::Socketrpc::ErrorReason.name_for_tag(res.code).to_s.downcase
-              error_class = Protobuf::Rpc.const_get(error_reason.camelize)
+            when ::Protobuf::Error, ::Protobuf::Rpc::ClientError
+              error_reason = ::Protobuf::Socketrpc::ErrorReason.name_for_tag(res.code).to_s.downcase
+              error_class = ::Protobuf::Rpc.const_get(error_reason.camelize)
               error = error_class.new(res.message)
               raise_error(error, raise_error)
             else
@@ -109,13 +109,13 @@ module Protobuf
             names = self.class.name.split('::')
             msg_class_name = [names[0..-3], 'Messages', names[-1]].flatten.join('::')
 
-            msg = if args.first.is_a?(Protobuf::Message)
+            msg = if args.first.is_a?(::Protobuf::Message)
                     args.shift
                   elsif Object.const_defined?(msg_class_name, false)
                     msg_class = Object.const_get(msg_class_name)
                     msg_class.new(*args)
                   elsif args.first.respond_to?(:to_hash)
-                    Messages::RpcCompressedMessage.new(response_body: args.first.to_hash.to_json,
+                    ::Protobuf::Rpc::Messages::RpcCompressedMessage.new(response_body: args.first.to_hash.to_json,
                                                        serializer: :JSON,
                                                        compressed: false)
                   else
